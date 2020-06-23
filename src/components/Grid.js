c0ed418d2de9262b9eeebc14ac3daf0195316c32
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-06-22 17:18:51 -0700

    cell selection toggled with click

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,21 +1,47 @@
 import React, { useState } from 'react';
-const numRows = 50;
-const numCols = 50;
+import { produce } from 'immer';
+
+const numRows = 3;
+const numCols = 3;
 
 const Grid = () => {
     // initiallizing with anon fn so it will only run once
-    const [grid, setGrid] = useState(() => {
-        const rows = [];
-        // for numRows, push array of numCols with anon fn setting vals as 0s
-        for (let i = 0; i < numRows; i++){
+    const buildGrid = () => {
+        const rows = []
+        for(let i = 0; i < numRows; i++){
             rows.push(Array.from(Array(numCols), () => 0))
         }
         return rows;
-    });
+    };
+    const [tempGrid, setTempGrid] = useState(buildGrid())
+    const [gen0, setGen0] = useState(tempGrid)
+    const [gen1, setGen1] = useState(gen0)
     
     return (
-      <div style={{display: 'grid', gridTemplateColumns: `repeat(${numCols}, 20px)`}}>
-        {grid.map((rows, ri) => rows.map((col, ci) => (<div style={{ width: 20, height: 20, backgroundColor: grid[ri][ci] ? 'green' : 'grey', border: "solid 1px black"}}/>)))}
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${numCols}, 20px)`
+        }}
+      >
+        {tempGrid.map((rows, i) =>
+          rows.map((col, j) => (
+            <div
+              onClick={() => 
+                {setGen0([tempGrid[i][j] = tempGrid[i][j] ? 0 : 1]);
+                 console.log(`${tempGrid}`)
+            }
+            }
+              key={`${i}-${j}`}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: tempGrid[i][j] ? 'green' : 'grey',
+                border: 'solid 1px black'
+              }}
+            />
+          ))
+        )}
       </div>
     );
 }
