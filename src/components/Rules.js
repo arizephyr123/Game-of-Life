8758b89076a43f933417ebf3b774c598f1cc7bce
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-11-26 22:34:12 -0800

    styling stucture in Grid,About,Rules

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -18,16 +18,18 @@ const Rules = props => {
           Rules
         </Button>
         {visible && (
-          <div>
+          <div className='info-box'>
             <Fade in={visible}>
-              <div id='conway-rules'>
-                Conway's Game of Life is a 2d visualization of how generations
-                of 'organizims' will survive and spread through a given space.
-                Ty given the following simplified rules: 1. If an organisim is
-                alive and has 2 or 3 neighbors, then it remains alive in the
-                next generation. Else it dies. 2. If an organisim is dead and
-                has exactly 3 neigbors, then it comes back to life in the next
-                generation. Else it stays dead
+              <div className='rules' id='conway-rules'>
+                <h5 className='info-text'>
+                  Conway's Game of Life is a 2d visualization of how generations
+                  of 'organizims' will survive and spread through a given space.
+                  Ty given the following simplified rules: 1. If an organisim is
+                  alive and has 2 or 3 neighbors, then it remains alive in the
+                  next generation. Else it dies. 2. If an organisim is dead and
+                  has exactly 3 neigbors, then it comes back to life in the next
+                  generation. Else it stays dead
+                </h5>
               </div>
             </Fade>
           </div>
