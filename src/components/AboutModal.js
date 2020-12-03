a25b048e1d87a3904e1f41b2103a98e9c3dfc5f3
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-12-02 23:15:35 -0800

    reactstrap info modals WIP

--- src/components/AboutModal.js
+++ src/components/AboutModal.js
@@ -9,6 +9,7 @@ const AboutModal = props => {
 
   return (
     <Modal {...props} aria-labelledby='contained-modal-title-vcenter'>
+      {console.log(props)}
       <Modal.Header closeButton>
         <Modal.Title id='contained-modal-title-vcenter'>
           Using Grid in Modal
@@ -16,26 +17,13 @@ const AboutModal = props => {
       </Modal.Header>
       <Modal.Body className='show-grid'>
         <Container>
-          <Row>
-            <Col xs={12} md={8}>
-              .col-xs-12 .col-md-8
-            </Col>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
-            </Col>
-          </Row>
-
-          <Row>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
-            </Col>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
-            </Col>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
-            </Col>
-          </Row>
+          <h5 className='info-text'>
+            // Cellular Automata(CA) programs are used in research setings to //
+            model ecological, epidemiological and chemical simulations as //
+            well as CA-based computer processors. In software engineering, //
+            Turing Complete describes a computational system that has //
+            equivilent processing power as a Turing Machine. //{' '}
+          </h5>
         </Container>
       </Modal.Body>
       <Modal.Footer>
