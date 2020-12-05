17a2644cb027ee9d127e49444cbf6f96988c2b48
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-12-04 20:49:42 -0800

    About, Rules button, text changes

--- src/components/AboutModal.js
+++ src/components/AboutModal.js
@@ -1,28 +1,33 @@
-import React, { useState } from 'react';
-import { Button, Modal, Row, Col, Container } from 'react-bootstrap';
+import React from 'react';
+import { Button, Modal, Container } from 'react-bootstrap';
 
 const AboutModal = props => {
-  //   const [visible, setVisible] = useState(false);
-  //   const handleClick = () => {
-  //     setVisible(!visible);
-  //   };
-
   return (
     <Modal {...props} aria-labelledby='contained-modal-title-vcenter'>
       {console.log(props)}
       <Modal.Header closeButton>
         <Modal.Title id='contained-modal-title-vcenter'>
-          Using Grid in Modal
+          About Conway's Game of Life
         </Modal.Title>
       </Modal.Header>
       <Modal.Body className='show-grid'>
         <Container>
           <h5 className='info-text'>
-            // Cellular Automata(CA) programs are used in research setings to //
-            model ecological, epidemiological and chemical simulations as //
-            well as CA-based computer processors. In software engineering, //
-            Turing Complete describes a computational system that has //
-            equivilent processing power as a Turing Machine. //{' '}
+            Conway's Game of Life is a visualization of how generations of
+            'organizims' will survive and spread through a given space.
+          </h5>
+          <h5 className='info-text'>
+            Cellular automation programs are used in research settings to model
+            ecological, epidemiological and chemical simulations as well as
+            cellular-automation-based computer processors. A cellular automation
+            is a grid of cells that have a finite number of states and
+            dimensions. Each cell's state is determined by the state of it's
+            neighbors in the previous generation as dictated by the rules of the
+            automation.
+          </h5>
+          <h5 className='info-text'>
+            In software engineering, Turing Complete describes a computational
+            system that has equivilent processing power as a Turing Machine.
           </h5>
         </Container>
       </Modal.Body>
